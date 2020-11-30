cbbf8efe024a2544f92e1ff1218b3798f8fe2467
LHY <847798512@qq.com>
2020-11-30 10:39:37 +0800

    基本完成numberpad逻辑

--- src/components/MoneyNumberPad.vue
+++ src/components/MoneyNumberPad.vue
@@ -1,52 +1,79 @@
 <template>
     <div class="numberPad">
         {{output}}
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
+        <button @click="input">1</button>
+        <button @click="input">2</button>
+        <button @click="input">3</button>
         <button>
             <svg class="icon" aria-hidden="true">
                 <use xlink:href="#icon-date"></use>
             </svg>
-            今天
         </button>
         <button @click="input">4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>+</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>-</button>
-        <button>.</button>
-        <button>0</button>
-        <button>
+        <button @click="input">5</button>
+        <button @click="input">6</button>
+
+        <button @click="remove">
             <svg class="icon" aria-hidden="true">
                 <use xlink:href="#icon-delete"></use>
             </svg>
         </button>
+        <button @click="input">7</button>
+        <button @click="input">8</button>
+
+        <button @click="input">9</button>
         <button>完成</button>
+
+
     </div>
 </template>
 
 <script lang="ts">
     import Vue from 'vue';
     import {Component} from "vue-property-decorator";
+
     @Component
-    export default class MoneyNumberPad extends Vue{
-        output='';
-        input(event: MouseEvent){
-            const button = (event.target as HTMLButtonElement)
-            this.output += button.textContent
+    export default class MoneyNumberPad extends Vue {
+        output = '0';
+
+        input(event: MouseEvent) {
+            const button = (event.target as HTMLButtonElement);
+            const input = button.textContent as string;
+            if (this.output.length === 16) {
+                return;
+            }
+            if (this.output === '0') {
+                if ('0123456789'.indexOf(input) >= 0) {
+                    this.output = input
+                } else {
+                    this.output += input
+                }
+                return
+            }
+            if (this.output.indexOf('.') >= 0 && input === '.') {
+                return;
+            }
+            this.output += input
+
+        }
+
+        remove() {
+            if (this.output.length === 1) {
+                this.output = '0'
+            } else {
+                this.output = this.output.substring(0, this.output.length - 1)
+            }
+
         }
     }
 </script>
 
 <style scoped lang="scss">
-    .icon{
+    .icon {
         width: 1.5em;
         height: 1.5em;
     }
+
     .numberPad {
         > button {
             font-size: 20px;
